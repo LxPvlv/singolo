85ec101479cc35433670a626fc0e862bfda1dd87
lxpvlv <w.lxpvlv@gmail.com>
2020-03-12 21:01:52 +0500

    feat: add scroll navigation

---
+++ script.js
@@ -0,0 +1,19 @@
+const headerNavigation = document.querySelector("#header-navigation");
+
+headerNavigation.addEventListener("click", evt => {
+  const target = evt.target;
+  if(target.tagName !== 'A') return
+
+  evt.preventDefault();
+
+  [...evt.currentTarget.children].forEach(node =>
+    node.classList.remove("active")
+  );
+
+  target.parentElement.classList.add("active");
+
+  const destination = document.querySelector(target.getAttribute("href"))
+  destination && destination.scrollIntoView({
+    behavior: "smooth"
+  });
+});
