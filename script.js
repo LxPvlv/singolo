5dc2c0048f0a080548c38995c250f92709f78d07
lxpvlv <w.lxpvlv@gmail.com>
2020-03-17 00:25:05 +0500

    feat: remake slider with circular linked list

--- script.js
+++ script.js
@@ -19,57 +19,91 @@ headerNavigation.addEventListener("click", evt => {
     });
 });
 
-// Slider
-const slider = document.querySelector("#slider");
-const duration = getComputedStyle(slider).transitionDuration;
-const slidesContainer = slider.querySelector(".slides-container");
-let elementSource = slidesContainer.firstElementChild;
-let elementDestination = slidesContainer.lastElementChild;
-let isAnimated = false;
+//slider
+class Slide {
+  constructor(slide) {
+    this.slide = slide;
+    this.prev = null;
+    this.next = null;
+  }
+}
 
-slider.addEventListener("click", evt => {
-  const target = evt.target;
-  const { direction } = target.dataset;
-  if (!direction) return;
-  if (isAnimated) return;
+class SlidesList {
+  constructor(slider) {
+    this.slider = slider;
+    const slidesContainer = slider.querySelector(".slides-container");
+    const slides = slidesContainer.children;
 
-  switch (direction) {
-    case "next":
-      slidesContainer.append(elementDestination);
-      slidesContainer.style.transition = `transform ${duration}`;
-      slidesContainer.style.transform = "translateX(-100%)";
-      break;
-    case "prev":
-      requestAnimationFrame(() => {
-        slidesContainer.prepend(elementDestination);
-        slidesContainer.style.transform = "translateX(-100%)";
-        requestAnimationFrame(() => {
-          slidesContainer.style.transition = `transform ${duration}`;
-          slidesContainer.style.transform = "translateX(0%)";
-        });
-      });
-      break;
+    const list = [...slides].reduce((list, node) => list.add(node), this);
+    this.head = list.head.next;
+
+    this.dict = {
+      next: "right",
+      prev: "left"
+    }
+
+    this.isAnimated = false;
   }
 
-  const { bgcolor, bbcolor } = elementDestination.dataset;
-  slider.style.backgroundColor = bgcolor;
-  slider.style.borderBottomColor = bbcolor;
+  add(slide) {
+    const newNode = new Slide(slide);
 
-  isAnimated = true;
-  slidesContainer.addEventListener(
-    "transitionend",
-    evt => {
-      isAnimated = false;
-      slidesContainer.style.transition = "unset";
-      slidesContainer.style.transform = "translateX(0%)";
-      slidesContainer.prepend(elementDestination);
-      [elementSource, elementDestination] = [elementDestination, elementSource];
-    },
-    { once: true }
-  );
+    if (!this.head) {
+      this.head = newNode;
+      newNode.next = newNode;
+      newNode.prev = newNode;
+      return this;
+    } else {
+      const next = this.head.next;
+      this.head.next = newNode;
+      newNode.prev = this.head;
+      newNode.next = next;
+      next.prev = newNode;
+      this.head = newNode;
+      return this;
+    }
+  }
+
+  move(direction) {
+    if (this.isAnimated) return;
+
+    if (!direction) return;
+    const offset = this.dict[direction]
+
+    this.isAnimated = true;
+
+    let from = this.head.slide;
+    let to = this.head[direction].slide;
+    from.classList.add("active-slide", `move-${direction}`);
+    to.classList.add("next-slide", `move-${direction}`);
+    to.style[offset] = "-100%";
+    this.slider.style.backgroundColor = to.dataset.bgcolor;
+    this.slider.style.borderBottomColor = to.dataset.bbcolor;
+    to.addEventListener(
+      "animationend",
+      evt => {
+        this.isAnimated = false;
+        from.classList.remove("active-slide", `move-${direction}`);
+        to.classList.remove("next-slide", `move-${direction}`);
+        to.classList.add("active-slide");
+        to.style[offset] = "";
+        this.head = this.head[direction];
+      },
+      { once: true }
+    );
+  }
+}
+
+const slider = document.querySelector("#slider");
+
+const list = new SlidesList(slider);
+
+slider.addEventListener("click", evt => {
+  list.move(evt.target.dataset.direction);
 });
 
 //btn-power
+const slidesContainer = slider.querySelector(".slides-container");
 const phones = slidesContainer.querySelectorAll(".btn-power");
 phones.forEach(phone => {
   phone.addEventListener("click", evt => {
